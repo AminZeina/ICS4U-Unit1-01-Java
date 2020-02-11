714ecaf276bdbf29f30f849dd6f3771004821ac9
CitrusMelon <amin.zeina@ocsbstudent.ca>
2020-02-11 17:48:00 +0000

    init

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * This program prints Hello World to console.
+ *  
+ * @author Amin Zeina
+ * @version 1.0
+ * @since 2020-02-11
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
